b8c2f7e94579d3d12bd1cfc02110f979734b210d
Janindu-Pathirana <78893460+Janindu-Pathirana@users.noreply.github.com>
2023-03-10 10:36:49 +0530

    add

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"go/types"
+
+	"github.com/portto/solana-go-sdk/client"
+	"github.com/portto/solana-go-sdk/rpc"
+)
+
+type Metadata struct {
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Image       string   `json:"image"`
+	Attributes  []string `json:"attributes,omitempty"`
+}
+
+type Contract struct {
+	client      *client.Client
+	programID   types
+	wallet      solana.Signer
+	token       solana.PublicKey
+	tokenAmount uint64
+}
+
 func main() {
+	// create a RPC client
+	c := client.NewClient(rpc.DevnetRPCEndpoint)
 
 }
